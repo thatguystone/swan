5eed06a00c31fc7162cd88dc6fb85a12c89ffad4
Andrew Stone <andrew@clovar.com>
2015-02-04 12:30:19 -0500

    Complete title test cases

--- extract_title_test.go
+++ extract_title_test.go
@@ -1,1 +1,21 @@
 package swan
+
+import (
+	"testing"
+)
+
+func TestTitlePyExtractor(t *testing.T) {
+	t.Parallel()
+
+	runPyTests(t,
+		"test_data/python-goose/title/",
+		func(t *testing.T, name string, a *Article, r *Result) {
+			if a.Meta.Title != r.Expected.Title {
+				t.Fatalf(
+					"%s: Title does not match:\n"+
+						"	Got:      %s\n"+
+						"	Expected: %s",
+					name, a.Meta.Title, r.Expected.Title)
+			}
+		})
+}
